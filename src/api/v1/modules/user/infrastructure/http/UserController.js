a4beb2c39a73803e237666dd638d84b2b53dccb6
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2025-10-29 21:38:10 +0530

    fix the server

--- src/api/v1/modules/user/infrastructure/http/UserController.js
+++ src/api/v1/modules/user/infrastructure/http/UserController.js
@@ -1,7 +1,7 @@
 /**
  * User Controller
  */
-import { asyncHandler, HTTP_STATUS } from '../../../../../core/http/index.js';
+import { asyncHandler, HTTP_STATUS } from '../../../../../../core/http/index.js';
 
 class UserController {
   constructor(userService, logger) {
